f42311ed0730ff47689430359ddef9ccdc9e798b
Yuto Mori <mt09forest.pc@gmail.com>
2019-10-21 01:27:27 +0900

    wip

--- abc143/b copy.py
+++ abc143/b copy.py
@@ -3,7 +3,7 @@ from scipy.special import comb
 N=int(input())
 d_list=[int(i) for i in input().split()]
 # comb = list(itertools.combinations(d_list, 2))
-comb_list = comb(d_list, 2, exact=True)
+comb_list = comb(len(d_list), 2, exact=True)
 sum_heal = 0
 for i,j in comb:
     sum_heal += i*j
