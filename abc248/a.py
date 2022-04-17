7055fd91f9848852fab29edf02c6fb239788b417
Yuto Mori <tmori9banner@gmail.com>
2022-04-17 19:51:00 +0900

    change python version

---
+++ abc248/a.py
@@ -0,0 +1,7 @@
+a = input()
+all_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+for one_str in all_list:
+    if not one_str in a:
+        print(one_str)
---
+++ abc248/b.py
@@ -0,0 +1,5 @@
+import math
+a, b, k = map(int, input().split())
+
+
+print(math.ceil(math.log((b/a),k)))
